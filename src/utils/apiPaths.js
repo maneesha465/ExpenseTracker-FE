7447e6f747a5f6d66ae02e7cf00a6f294974b3c3
maneesha465 <maneesha011235@gmail.com>
2025-08-19 19:10:03 +0530

    change api

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,6 +1,6 @@
 import { FaRegistered } from "react-icons/fa";
 
-export const BASE_URL = " http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 //utils/apiPaths.js
 
